c46ad987d68056b50e1c9c635f131e37c8b89989
Guo Zhang <zhangguo@quanttide.com>
2022-04-12 00:16:00 +0800

    feature: MarkdownCodeExecutingBuilder重新定义为抽象类

--- example/lib/routes/notebook.dart
+++ example/lib/routes/notebook.dart
@@ -2,6 +2,7 @@
 
 import "package:flutter/material.dart";
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_highlight/flutter_highlight.dart';
 
 import 'package:cloud_ide_widgets/cloud_ide_widgets.dart';
 
@@ -25,7 +26,7 @@ class NotebookRoute extends StatelessWidget {
         data: markdownCodeText,
         selectable: true,
         builders: {
-          'code': MarkdownCodeExecutingBuilder(
+          'code': CustomMarkdownCodeExecutingBuilder(
               CloudExecutor(
                 host: EnvironmentConfig.executorHost,
                 path: EnvironmentConfig.executorPath,
@@ -34,4 +35,60 @@ class NotebookRoute extends StatelessWidget {
         }
     );
   }
+}
+
+
+class CustomMarkdownCodeExecutingBuilder extends MarkdownCodeExecutingBuilder {
+  CustomMarkdownCodeExecutingBuilder(CloudExecutor executor) : super(executor);
+
+  /// 渲染输入
+  ///
+  /// 参考资料：
+  ///   - https://pub.dev/packages/flutter_highlight#-readme-tab-
+  @override
+  Widget buildInput(String input){
+    return Container(
+        decoration: BoxDecoration(
+          // 黑框
+          border: Border.all(),
+        ),
+        child: HighlightView(
+          // 代码高亮
+          input,
+          language: 'python',
+        )
+    );
+  }
+
+  /// 渲染输出
+  @override
+  Widget buildOutput(String output){
+    return Text(output);
+  }
+
+  /// 渲染输出前的加载状态
+  @override
+  Widget buildProgressiveOutput(){
+    return const Text('代码运行中...');
+  }
+
+  /// 渲染异常输出
+  @override
+  Widget buildErrorOutput(e){
+    return Text("代码运行异常：\n$e");
+  }
+
+  /// 渲染输入组件和输出组件的布局
+  /// 如果需要可以覆盖默认方法进行自定义
+  /*
+  @override
+  Widget buildLayout(Widget inputWidget, Widget outputWidget){
+    return Column(
+      children: [
+        inputWidget,
+        outputWidget,
+      ],
+    );
+  }
+   */
 }
--- lib/src/notebooks.dart
+++ lib/src/notebooks.dart
@@ -8,55 +8,32 @@ import "package:flutter/material.dart";
 
 import "package:flutter_markdown/flutter_markdown.dart";
 import "package:markdown/markdown.dart" as md;
-import 'package:flutter_highlight/flutter_highlight.dart';
 
 import "executors.dart";
 
 
-/// Markdown代码运行
+/// Markdown代码运行渲染器抽象类
 ///
-/// 用例：
-///
-/// ```flutter
-/// Markdown(
-///   ...
-///   builders: {
-///     'code': MarkdownCodeExecutingBuilder(executor),
-///   }
-/// )
-/// ```
-///
-/// 参考资料：
-///   - 最佳实践：https://stackoverflow.com/questions/59592640/how-to-add-code-syntax-highlighter-to-flutter-markdown
-class MarkdownCodeExecutingBuilder extends MarkdownElementBuilder {
+/// TODO: 增加编辑输入功能。
+abstract class MarkdownCodeExecutingBuilder extends MarkdownElementBuilder {
   /// 代码运行器实例
   CloudExecutor executor;
 
   MarkdownCodeExecutingBuilder(this.executor);
 
   /// 渲染输入
-  ///
-  /// 参考资料：
-  ///   - https://pub.dev/packages/flutter_highlight#-readme-tab-
-  Widget buildInput(String input){
-    return Container(
-      decoration: BoxDecoration(
-        // 黑框
-        border: Border.all(),
-      ),
-      child: HighlightView(
-        // 代码高亮
-        input,
-        language: 'python',
-      )
-    );
-  }
+  Widget buildInput(String input);
 
   /// 渲染输出
-  Widget buildOutput(String output){
-    return Text(output);
-  }
+  Widget buildOutput(String output);
 
+  /// 渲染输出前的加载状态
+  Widget buildProgressiveOutput();
+
+  /// 渲染异常输出
+  Widget buildErrorOutput(e);
+
+  /// 渲染输入组件和输出组件的布局
   Widget buildLayout(Widget inputWidget, Widget outputWidget){
     return Column(
       children: [
@@ -66,8 +43,16 @@ class MarkdownCodeExecutingBuilder extends MarkdownElementBuilder {
     );
   }
 
-  /// 渲染输入和输出
-  Widget? build(String input){
+  /// 运行输入得到输出并渲染
+  ///
+  /// 参考资料：
+  ///   - API文档：https://pub.dev/documentation/flutter_markdown/latest/flutter_markdown/MarkdownElementBuilder/visitElementAfter.html
+  ///   - 样例：https://github.com/flutter/packages/blob/main/packages/flutter_markdown/example/lib/demos/subscript_syntax_demo.dart#L147
+  @override
+  Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    // 输入
+    String input = element.textContent;
+    // 运行和渲染输出
     return buildLayout(
       // 输入组件
       buildInput(input),
@@ -80,31 +65,17 @@ class MarkdownCodeExecutingBuilder extends MarkdownElementBuilder {
             // 请求已结束
             if (snapshot.hasError) {
               // 请求失败，显示错误
-              return buildOutput("Error: ${snapshot.error}");
+              return buildErrorOutput(snapshot.error);
             } else {
-            // 请求成功，显示数据
-            return buildOutput("${snapshot.data}");
+              // 请求成功，显示数据
+              return buildOutput(snapshot.data);
             }
           } else {
             // 请求未结束，显示正在加载的状态
-            return buildOutput('代码运行中...');
+            return buildProgressiveOutput();
           }
         },
       ),
     );
-  }
-
-  /// 运行输入得到输出结果，输入和输出为两个组件分别渲染。
-  ///
-  /// 参考资料：
-  ///   - API文档：https://pub.dev/documentation/flutter_markdown/latest/flutter_markdown/MarkdownElementBuilder/visitElementAfter.html
-  ///   - 样例：https://github.com/flutter/packages/blob/main/packages/flutter_markdown/example/lib/demos/subscript_syntax_demo.dart#L147
-  @override
-  // ignore: body_might_complete_normally_nullable
-  Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
-    // 输入
-    String input = element.textContent;
-    // 运行和渲染输出
-    return build(input);
   }
 }
